7d26ad3ba638e9c260d82e7d5c5f4fc3790e82fc
Ken-S039 <s0624039@iwate-u.ac.jp>
2026-08-13 16:38:09 +0900

    kengo create Goal

---
+++ 2026SummerGame/Assets/NormalGoal.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalGoal : BasicGoal
+{
+    protected override bool CanGoal()
+    {
+        return true;
+    }
+}
